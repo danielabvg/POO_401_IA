a0be69b9f5176b95e226d500ce440fadd219989a
danielabvg <danielabvg@outlook.com>
2026-03-26 07:47:48 -0600

    version3 implementacion del procesador

--- src/main/java/com/mx/curso/Unidad2/Practica05/Main.java
+++ src/main/java/com/mx/curso/Unidad2/Practica05/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        String texto = "El producto es excelente, muy bueno y genial.";
+        String texto = "El producto es excelente, muy bueno e interesante.";
 
         List<ProcesadorNLP> procesadores = new ArrayList<>();
 
