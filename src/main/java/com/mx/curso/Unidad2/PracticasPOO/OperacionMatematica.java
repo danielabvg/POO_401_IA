02f3656e9d43a7493345b06e7b4fc3f3bb8043c4
danielabvg <danielabvg@outlook.com>
2026-04-15 08:04:18 -0600

    Sentencias y expresiones básicas

---
+++ src/main/java/com/mx/curso/Unidad2/PracticasPOO/OperacionMatematica.java
@@ -0,0 +1,20 @@
+package com.mx.curso.Unidad2.PracticasPOO;
+
+public class OperacionMatematica {
+
+    public static void main(String[] args) {
+
+        // Declaración + asignación con expresión
+        int producto = 8 * 12;
+
+        // Declaración
+        int cociente;
+
+        // Asignación con expresión
+        cociente = 24 / 6;
+
+        // Salida
+        System.out.println("El producto es: " + producto);
+        System.out.println("El cociente es: " + cociente);
+    }
+}
